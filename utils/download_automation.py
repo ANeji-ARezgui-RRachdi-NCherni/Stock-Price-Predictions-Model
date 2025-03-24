d3af03c89551d1b061e1121a1c49543fe09279d7
rezgu <rezgui012@gmail.com>
2025-03-24 14:09:41 +0100

    download automation code refactoring

--- utils/download_automation.py
+++ utils/download_automation.py
@@ -4,124 +4,39 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
-# Replace with the actual path to your msedgedriver executable
+#constants
+LINK =  "https://www.ilboursa.com/marches/download/"
+START_DATE = "01/01/2010"
+
+# browser webdriver
 edgedriver_path = "C:/Users/rezgu/webdriver/msedgedriver.exe"
 
 # Set up EdgeDriver
 service = Service(executable_path=edgedriver_path)
 driver = webdriver.Edge(service=service)
+
+# Stock Symbols
 links=[
-    "https://www.ilboursa.com/marches/download/HL",
-    "https://www.ilboursa.com/marches/download/GIF",
-    "https://www.ilboursa.com/marches/download/ECYCL",
-    "https://www.ilboursa.com/marches/download/SOKNA",
-    "https://www.ilboursa.com/marches/download/NAKL",
-    "https://www.ilboursa.com/marches/download/LSTR",
-    "https://www.ilboursa.com/marches/download/ELBEN",
-    "https://www.ilboursa.com/marches/download/DH",
-    "https://www.ilboursa.com/marches/download/CITY",
-    "https://www.ilboursa.com/marches/download/SCB",
-    "https://www.ilboursa.com/marches/download/CIL",
-    "https://www.ilboursa.com/marches/download/CREAL",
-    "https://www.ilboursa.com/marches/download/CELL",
-    "https://www.ilboursa.com/marches/download/CC",
-    "https://www.ilboursa.com/marches/download/BTE",
-    "https://www.ilboursa.com/marches/download/BIAT",
-    "https://www.ilboursa.com/marches/download/BHL",
-    "https://www.ilboursa.com/marches/download/BH",
-    "https://www.ilboursa.com/marches/download/BHASS",
-    "https://www.ilboursa.com/marches/download/BL",
-    "https://www.ilboursa.com/marches/download/BNA",
-    "https://www.ilboursa.com/marches/download/BT",
-    "https://www.ilboursa.com/marches/download/TJARI",
-    "https://www.ilboursa.com/marches/download/TJL",
-    "https://www.ilboursa.com/marches/download/AST",
-    "https://www.ilboursa.com/marches/download/ASSMA",
-    "https://www.ilboursa.com/marches/download/ASSAD",
-    "https://www.ilboursa.com/marches/download/ARTES",
-    "https://www.ilboursa.com/marches/download/ATL",
-    "https://www.ilboursa.com/marches/download/ATB",
-    "https://www.ilboursa.com/marches/download/AMS",
-    "https://www.ilboursa.com/marches/download/AMI",
-    "https://www.ilboursa.com/marches/download/AB",
-    "https://www.ilboursa.com/marches/download/AL",
-    "https://www.ilboursa.com/marches/download/AETEC",
-    "https://www.ilboursa.com/marches/download/ADWYA",
+    "HL","GIF","ECYCL","SOKNA","NAKL","LSTR","ELBEN","DH","CITY","SCB","CIL","CREAL",
+    "CELL","CC","BTE","BIAT","BHL","BH","BHASS","BL","BNA","BT","TJARI","TJL","AST",
+    "ASSMA","ASSAD","ARTES","ATL","ATB","AMS","AMI","AB","AL","AETEC","ADWYA",
 ]
 
-# Define 3-month date ranges from 1st Jan 2010 to 4th Mar 2025 in dd/mm/yyyy format
-date_ranges = [
-    ("01/01/2010", "31/03/2010"),
-    ("01/04/2010", "30/06/2010"),
-    ("01/07/2010", "30/09/2010"),
-    ("01/10/2010", "31/12/2010"),
-    ("01/01/2011", "31/03/2011"),
-    ("01/04/2011", "30/06/2011"),
-    ("01/07/2011", "30/09/2011"),
-    ("01/10/2011", "31/12/2011"),
-    ("01/01/2012", "31/03/2012"),
-    ("01/04/2012", "30/06/2012"),
-    ("01/07/2012", "30/09/2012"),
-    ("01/10/2012", "31/12/2012"),
-    ("01/01/2013", "31/03/2013"),
-    ("01/04/2013", "30/06/2013"),
-    ("01/07/2013", "30/09/2013"),
-    ("01/10/2013", "31/12/2013"),
-    ("01/01/2014", "31/03/2014"),
-    ("01/04/2014", "30/06/2014"),
-    ("01/07/2014", "30/09/2014"),
-    ("01/10/2014", "31/12/2014"),
-    ("01/01/2015", "31/03/2015"),
-    ("01/04/2015", "30/06/2015"),
-    ("01/07/2015", "30/09/2015"),
-    ("01/10/2015", "31/12/2015"),
-    ("01/01/2016", "31/03/2016"),
-    ("01/04/2016", "30/06/2016"),
-    ("01/07/2016", "30/09/2016"),
-    ("01/10/2016", "31/12/2016"),
-    ("01/01/2017", "31/03/2017"),
-    ("01/04/2017", "30/06/2017"),
-    ("01/07/2017", "30/09/2017"),
-    ("01/10/2017", "31/12/2017"),
-    ("01/01/2018", "31/03/2018"),
-    ("01/04/2018", "30/06/2018"),
-    ("01/07/2018", "30/09/2018"),
-    ("01/10/2018", "31/12/2018"),
-    ("01/01/2019", "31/03/2019"),
-    ("01/04/2019", "30/06/2019"),
-    ("01/07/2019", "30/09/2019"),
-    ("01/10/2019", "31/12/2019"),
-    ("01/01/2020", "31/03/2020"),
-    ("01/04/2020", "30/06/2020"),
-    ("01/07/2020", "30/09/2020"),
-    ("01/10/2020", "31/12/2020"),
-    ("01/01/2021", "31/03/2021"),
-    ("01/04/2021", "30/06/2021"),
-    ("01/07/2021", "30/09/2021"),
-    ("01/10/2021", "31/12/2021"),
-    ("01/01/2022", "31/03/2022"),
-    ("01/04/2022", "30/06/2022"),
-    ("01/07/2022", "30/09/2022"),
-    ("01/10/2022", "31/12/2022"),
-    ("01/01/2023", "31/03/2023"),
-    ("01/04/2023", "30/06/2023"),
-    ("01/07/2023", "30/09/2023"),
-    ("01/10/2023", "31/12/2023"),
-    ("01/01/2024", "31/03/2024"),
-    ("01/04/2024", "30/06/2024"),
-    ("01/07/2024", "30/09/2024"),
-    ("01/10/2024", "31/12/2024"),
-    ("01/01/2025", "04/03/2025")
-]
 #loop through all the links
 for link in links:
 
     # Open the target webpage for each date range
-    driver.get(link)
+    driver.get(LINK+link)
 
-    # Loop through each date range to perform the download
-    for start_date, end_date in date_ranges:
+
+    start_date = datetime.strptime(START_DATE, "%d/%m/%Y").date()
+    #add a 3 month period
+    end_date = start_date + relativedelta(months= 3)
+
+    while datetime.today().date() > end_date:    
         
         # Wait until the date pickers are available (adjust selectors as needed)
         wait = WebDriverWait(driver, 10)
@@ -130,10 +45,10 @@ for link in links:
         
         # Clear any existing values and input the new dates
         start_picker.clear()
-        start_picker.send_keys(start_date)
+        start_picker.send_keys(start_date.strftime("%d/%m/%Y"))
         
         end_picker.clear()
-        end_picker.send_keys(end_date)
+        end_picker.send_keys(end_date.strftime("%d/%m/%Y"))
         
         # Optional pause to let the page update based on new dates
         time.sleep(1)
@@ -144,6 +59,10 @@ for link in links:
         
         # Wait for the download to complete (adjust the duration as necessary)
         time.sleep(3)
+        
+        #update the start and end dates
+        start_date = end_date
+        end_date = start_date + relativedelta(months= 3)
 
 # Close the browser after processing all date ranges
 driver.quit()
