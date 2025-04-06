9998147f7c14bf5100fe37c8d2ab14e81b371938
Mohamed Anas Neji <mohamedanas.neji@medius.com>
2025-04-06 16:13:22 +0100

    swap order of imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from .download_automation import get_dates, update_dates, download_data
 from .constants import HEADERS, SYMBOLS, RAW_DATA_DOWNLOAD_BASELINK
+from .download_automation import get_dates, update_dates, download_data
 from .train_test_utils import split_dataset, get_target_from_dataset, get_features_from_dataset, train, evaluate, plot_evaluation_result
