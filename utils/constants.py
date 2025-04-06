e5c716559fd4b26d96a87b86fb796b556da419f1
anasneji2002 <123624693+anasneji2002@users.noreply.github.com>
2025-04-06 17:13:43 +0100

    feat: Add data Extraction pipeline (#7)
    Co-authored-by: rezgu <rezgui012@gmail.com>
    Co-authored-by: Ahmed Rezgui <ahmed.rezgui@insat.ucar.tn>

---
+++ utils/constants.py
@@ -0,0 +1,5 @@
+RAW_DATA_DOWNLOAD_BASELINK =  "https://www.ilboursa.com/marches/download/"
+HEADERS = "symbole;date;ouverture;haut;bas;cloture;volume"
+SYMBOLS= [ "HL","GIF","ECYCL","SOKNA","NAKL","LSTR","ELBEN","DH","CITY","SCB","CIL","CREAL",
+    "CELL","CC","BTE","BIAT","BHL","BH","BHASS","BL","BNA","BT","TJARI","TJL","AST",
+    "ASSMA","ASSAD","ARTES","ATL","ATB","AMS","AMI","AB","AL","AETEC","ADWYA"] 
