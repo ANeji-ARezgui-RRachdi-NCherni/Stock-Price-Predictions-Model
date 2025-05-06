69267aaf9e3cf94c1319ab547b6e19c976d374f6
Ahmed Rezgui <ahmed.rezgui@insat.ucar.tn>
2025-05-06 16:05:14 +0100

    Feat/rag system (#20)

--- utils/constants.py
+++ utils/constants.py
@@ -3,3 +3,5 @@ HEADERS = "symbole;date;ouverture;haut;bas;cloture;volume"
 SYMBOLS= [ "HL","GIF","ECYCL","SOKNA","NAKL","LSTR","ELBEN","DH","CITY","SCB","CIL","CREAL",
     "CELL","CC","BTE","BIAT","BHL","BH","BHASS","BL","BNA","BT","TJARI","TJL","AST",
     "ASSMA","ASSAD","ARTES","ATL","ATB","AMS","AMI","AB","AL","AETEC","ADWYA"] 
+NEWS_BASE_URL = "https://www.ilboursa.com"
+PAGE_URL= NEWS_BASE_URL+"/marches/actualites_bourse_tunis"
